c9d915425d9e660a183338fb5a8143b4415738cc
Drogon <aatrox48@gmail.com>
2022-01-11 20:01:17 -0600

    made getCurrentWeather function with hardcoded fetch

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,26 @@
 var searchFormEl = document.querySelector("#search");
 
 var getCurrentWeather = function() {
-    var apiUrl = ""
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.518589&lon=-86.810356&exclude=alerts,minutely&appid=d35dfaab0c2da2d1c3dcb0514127ce3f";
+
+    fetch(apiUrl)
+        .then(function(response) {
+            // request was successful
+            if (response.ok){
+                response.json().then(function(data) {
+                    console.log(data);
+                })
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        });
 };
+
+var displayCurrentWeather = function() {
+    
+};
+
+getCurrentWeather();
 
 // var formSubmitHandler = function(event) {
 //     // prevent page from refreshing
