41f78f7959d904486d2bacc4c8148fba94d296c6
Drogon <aatrox48@gmail.com>
2022-01-11 19:49:06 -0600

    added base files and html

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+var searchFormEl = document.querySelector("#search");
+
+var getCurrentWeather = function() {
+    var apiUrl = ""
+};
+
+// var formSubmitHandler = function(event) {
+//     // prevent page from refreshing
+//     event.preventDefault();
+
+//     // get value from input element
+//     var city = searchFormEl.value.trim();
+
+//     if (city) {
+//         getCurrentWeather(city);
+//     }
+// };
