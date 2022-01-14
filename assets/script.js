74c1c9f10174578e587a1a7f3a0f4ffba51a8b48
Drogon <aatrox48@gmail.com>
2022-01-13 19:24:57 -0600

    display current weather data

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,19 @@
 var searchFormEl = document.querySelector("#search");
 
 var getCurrentWeather = function() {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.518589&lon=-86.810356&exclude=alerts,minutely&appid=d35dfaab0c2da2d1c3dcb0514127ce3f";
+    // var lat 
+    // var lon
+
+
+
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.518589&lon=-86.810356&units=imperial&exclude=alerts,minutely&appid=d35dfaab0c2da2d1c3dcb0514127ce3f";
 
     fetch(apiUrl)
         .then(function(response) {
             // request was successful
             if (response.ok){
                 response.json().then(function(data) {
-                    console.log(data);
+                    displayCurrentWeather(data);
                 })
             } else {
                 alert("Error: " + response.statusText);
@@ -16,8 +21,27 @@ var getCurrentWeather = function() {
         });
 };
 
-var displayCurrentWeather = function() {
-    
+var displayCurrentWeather = function(data) {
+    var currentDate = document.querySelector('#city');
+    var currentTemperature = document.querySelector('#temp');
+    var currentWind = document.querySelector('#wind');
+    var currentHumiity = document.querySelector('#humidity');
+    var currentUV = document.querySelector('#uv');
+
+    currentDate.textContent = data.current.dt * 1000;
+    currentTemperature.textContent = "Temp: " + data.current.temp + " °F";
+    currentWind.textContent = "Wind: " + data.current.wind_speed + " MPH";
+    currentHumiity.textContent = "Humidity: " + data.current.humidity + " %";
+    currentUV.textContent = "UV Index: " + data.current.uvi;
+
+
+
+
+    // for(var i = 0; i < 5; i++) {
+    //     console.log(data.daily[i]);
+
+        
+    // };
 };
 
 getCurrentWeather();
